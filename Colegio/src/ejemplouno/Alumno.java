213dc2eaf6e7c069ae5862ae4e1ce943e6e1c3d2
victor <victor@gmail.com>
2015-04-16 17:11:08 +0200

    Primera version

--- Colegio/src/ejemplouno/Alumno.java
+++ Colegio/src/ejemplouno/Alumno.java
@@ -5,9 +5,11 @@ package ejemplouno;
 		/* Constructor */
 		protected String nombre;
 		protected int edad;
+		protected String apellido;
 		public Alumno(String a, int b){
 			nombre = a;
 			edad = b;
+			
 }
 		public int getEdad() {
 			return edad;
